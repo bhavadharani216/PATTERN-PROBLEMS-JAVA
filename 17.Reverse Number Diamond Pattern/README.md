5e9b604546499898f6e175c4cc53771ed82671e4
bhavadharani216 <bhavadharani216@gmail.com>
2026-01-26 17:06:24 +0530

    Reverse Number Diamond Pattern

---
+++ 17.Reverse Number Diamond Pattern/README.md
@@ -0,0 +1,20 @@
+# Reverse Number Diamond Pattern
+
+Problem:  
+Print a number pattern where digits increase in count and start in reverse order, then decrease symmetrically.
+
+Output:
+1
+21
+321
+4321
+54321
+4321
+321
+21
+1
+
+Concepts Used:
+- Nested for loops
+- Pattern symmetry
+- Number pattern logic
---
+++ 17.Reverse Number Diamond Pattern/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }System.out.println();}
+        
+        for(int i=1;i<n;i++){
+            for(int j=n-i;j>=1;j--){
+                System.out.print(j);
+            }System.out.println();
+        }
+    }
+}
