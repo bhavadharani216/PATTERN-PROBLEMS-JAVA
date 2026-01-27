9d3a982d7c3fd040edf9b43f53ebfbf8ba9a68a3
bhavadharani216 <bhavadharani216@gmail.com>
2026-01-27 18:47:30 +0530

    Centered Reverse Number Diamond

---
+++ 18.Centered Reverse Number Diamond/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ 
+public class Solution {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            for(int j=n-i+1;j<=n;j++){
+                System.out.print(j);
+            }System.out.println();}
+        
+        for(int i=1;i<n;i++){
+            for(int j=i+1;j<=n;j++){
+                System.out.print(j);
+            }System.out.println();
+        }
+    }
+}
