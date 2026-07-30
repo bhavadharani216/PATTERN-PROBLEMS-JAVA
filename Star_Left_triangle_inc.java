3f4a5334716d5ee8b05ba03e0e92118474587050
BHAVADHARANI GOVINDARAJ <147973983+bhavadharani216@users.noreply.github.com>
2026-07-30 10:39:58 +0530

    Create Star_Left_triangle_inc.java

---
+++ Star_Left_triangle_inc.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=1;i<=n;i++){
+            
+            for(int sp = 1;sp<=n-i;sp++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+} 
