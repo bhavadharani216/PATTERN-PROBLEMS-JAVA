04d91b4c8575b9acd429f6a797f8ef0f49379027
BHAVADHARANI GOVINDARAJ <147973983+bhavadharani216@users.noreply.github.com>
2026-07-30 11:02:01 +0530

    Create inverted pyramid.java

---
+++ inverted pyramid.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=1;i<=n;i++){
+            
+            for(int sp = 1;sp<=i-1;sp++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=2*(n-i)+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+} 
